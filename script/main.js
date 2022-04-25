5dae65a226efa39b39570d8ed7dfd8d9f6da34d9
Weritu <101119888+Weritu@users.noreply.github.com>
2022-04-25 09:02:58 -0300

    Adicionado Efeito vidro
    Adição do efeito vidro no index do site

--- script/main.js
+++ script/main.js
@@ -1,4 +1,4 @@
-function onloadAlert(){
-    alert("Site desenvolvido para a disciplina \n" +
-       "Programação Web Responsiva.");
-}
+// function onloadAlert(){
+//     alert("Site desenvolvido para a disciplina \n" +
+//        "Programação Web Responsiva.");
+// }
